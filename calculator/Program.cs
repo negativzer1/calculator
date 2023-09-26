910cf8d7c7a00759105961080c8c0d2a572f4e69
Anastasia <girl-physicist@mail.ru>
2023-09-26 17:02:36 +0300

    recomend

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -10,12 +10,12 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-
+// вынести реализацию в отдельный метод. в Main только его вызывать
             Console.WriteLine("введите первое число");
-            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number1 = Convert.ToInt32(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
             Console.WriteLine("введите второе число");
             int number2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите операцию, допустимые операции + - / *");
+            Console.WriteLine("Введите операцию, допустимые операции + - / *");// следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
             string a = Console.ReadLine();
             switch (a)
             {
