4db3f4b750d388f3d858f392ada7e02c9cbefbbd
name <name@DESKTOP-44JM4MC>
2023-09-26 16:34:23 +0300

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("введите первое число");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию, допустимые операции + - / *");
+            string a = Console.ReadLine();
+            switch (a)
+            {
+                case "+":
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case "-":
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case "*":
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case "/":
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    Console.WriteLine("операция неопределена");
+                    break;
+            }
+        }
+    }
+}
